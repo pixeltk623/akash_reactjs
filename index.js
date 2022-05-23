73d73060ee57aece77d1835ef55cd7f367b59843
pixeltk623 <86755498+pixeltk623@users.noreply.github.com>
2022-05-23 22:43:26 +0530

    Add files via upload
    Added

---
+++ index.js
@@ -0,0 +1,189 @@
+
+'use strict';
+
+//console.log('Hello This is Kumar');
+//console.warn('Hello This is Kumar');
+
+//alert('Sharvan Kumar');
+
+// document.write("<h1>Hello</h1>");
+
+// firstName // first_name // FirstName
+
+/*let n1 = parseInt(prompt("Enter Your First Number"));
+
+let n2 = parseInt(prompt("Enter Your Second Number"));
+
+console.log(typeof(n1));
+console.log(typeof(n2));*/
+
+
+// + - * / %
+
+// Add
+
+/*document.write("The N1 value is "+ n1+ "<br>");
+document.write("The N2 value is "+ n2+ "<br><br><br>");
+
+document.write(n1+n2+ "<br>");
+document.write(n1-n2+ "<br>");
+document.write(n1*n2+ "<br>");
+document.write((n1/n2).toFixed(3)+ "<br>");
+document.write(n1%n2+ "<br>");*/
+
+/*let 
+var 
+const*/
+
+
+
+
+// console.log(first_name);
+
+// console.log(last_name);
+
+/*{
+	var first_name = "Sharvan";
+	let last_name = "Kumar";	
+	console.log(first_name);
+	console.log(last_name);	
+}
+
+	console.log(first_name);
+	console.log(last_name);*/
+
+
+// const PI = 3.14;
+
+// PI = 8;
+
+// +
+// -
+// *
+// /
+// %
+
+// i++
+// i--
+// ++i
+// --i
+
+// +=
+// -=
+// /=
+// *=
+
+// >
+// <
+// >=
+// <=
+// =
+// ==
+// ===
+
+// !=
+// !==
+
+
+
+let x = 55;
+let y = 55;
+
+//console.log(x===y);
+//console.log(x>=y); //true
+//console.log(x<=y); // true
+//console.log(x>y); // false
+//console.log(x<y); // false
+
+
+console.log(++x); //56 //56
+
+console.log(x+1); //56 // 57
+
+
+
+
+console.log(x++); //56 // 58
+
+console.log(++x); //56 // 59
+console.log(++x); //56 //60
+
+console.log(x++); //56 // 61
+
+
+
+console.log(y++); //56 // 56
+console.log(++y); //56 // 57
+console.log(y+x); // 110 // 110 // 60 + 57 // 117
+
+// &&
+// OR ||
+
+/*!*/
+
+/*empty()
+isNaN()*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(PI);
